a1f531fe7ecd8c24ccd4fb6a57aa3dee9a9e3b5b
rpulkka <dreamspace2017@yandex.ru>
2019-11-08 16:49:30 +0200

    Week 2 exercises finished.

---
+++ viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,20 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class Viitegeneraattori implements ViitegeneraattoriInterface {
+
+    private int seuraava;
+
+    @Autowired
+    public Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    @Override
+    public int uusi(){
+        return seuraava++;
+    }
+}
